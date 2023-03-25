d797fe503e2d7251d73a8042afcfd0dfba067281
Paul Llanque <llanquepol@gmail.com>
2023-03-25 12:48:34 -0500

    update profile

--- src/Pages/Profile/Profile.jsx
+++ src/Pages/Profile/Profile.jsx
@@ -4,37 +4,8 @@ import { Link } from 'react-router-dom';
 import { StyledButton } from '../../components/Form/FormUI';
 import Input from '../../components/Input/Input';
 import { useAuth } from '../../context/AuthContext';
-import { colors, typography } from '../../styles';
+import { Name, TitleContainer, UserDataDiv } from './UI/ui';
 
-const UserDataDiv = styled.div`
-  background-color: ${colors.white};
-  display: flex;
-  flex-direction: column;
-  gap: 15px;
-  padding: 25px 14px;
-  box-shadow: 0px 10px 40px rgba(0, 0, 0, 0.03);
-  border-radius: 20px;
-`;
-
-const TitleContainer = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  margin: 30px 0;
-  padding: 2px;
-
-  h3 {
-    color: ${colors.black};
-    font-size: ${typography.text.lg};
-    font-weight: 500;
-  }
-`;
-
-const Name = styled.p`
-  ${typography.head.sm};
-  color: ${colors.black};
-  letter-spacing: 0.5px;
-`;
 
 const Profile = ({ onProfile }) => {
   const { user } = useAuth();
--- src/Pages/Profile/ProfilePage.jsx
+++ src/Pages/Profile/ProfilePage.jsx
@@ -1,6 +1,11 @@
+import styled from '@emotion/styled';
 import React, { useEffect, useState } from 'react';
 import Profile from './Profile';
 import UpdatePage from './UpdatePage';
+
+const ProfileContainer = styled.div`
+  padding: 0 25px;
+`;
 
 const ProfilePage = () => {
   const [page, setPage] = useState('profile');
@@ -12,11 +17,11 @@ const ProfilePage = () => {
   }
 
   return (
-    <>
+    <ProfileContainer>
       <h2>My Profile</h2>
       {page === 'profile' && <Profile onProfile={handlePage} />}
       {page === 'update' && <UpdatePage onUpdate={handlePage} />}
-    </>
+    </ProfileContainer>
   );
 };
 
